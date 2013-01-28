324861d03a6a43faedd84e489d1704b6a8affaef
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-28 11:02:30 +0100

    Hashdist profile build working; added config.yml

--- builder/builder.py
+++ builder/builder.py
@@ -5,21 +5,49 @@ import argparse
 from glob import glob
 
 from hashdist.deps import yaml
-from hashdist.core import load_configuration_from_inifile, SourceCache, BuildStore
+from hashdist.core import load_configuration_from_inifile, SourceCache, BuildStore, atomic_symlink
+from hashdist.core.fileutils import silent_makedirs
 from hashdist.hdist_logging import Logger, DEBUG, INFO
 
-# open "recipes.py" directly rather than using the Python package system
 from . import recipes as recipes_mod
 
-
-
 class Context(object):
-    def __init__(self, config, verbose, arch):
+    def __init__(self, config, verbose, arch, build_env):
         self.config = config
         self.logger = logger = Logger(DEBUG if verbose else INFO)
         self.source_cache = SourceCache.create_from_config(config, logger, create_dirs=True)
         self.build_store = BuildStore.create_from_config(config, logger, create_dirs=True)
         self.arch = arch
+        self.build_env = build_env
+        self.built = {}
+
+    def get_artifact_id(self, name):
+        return self.built[name][0]
+
+    def get_before_list(self, name):
+        return self.built[name][1]
+
+    def build_all(self, packages, root_name):
+        built = self.built # { name : (artifact_id, before_list) }
+        # depth-first traversal
+        def visit(name):
+            if name in built:
+                return built[name]
+            pkg = packages[name]
+            imports = [] # the imports to build this package
+            before = [] # to be given to dependee, which puts this in before-list for this artifact
+            for dep in pkg.get('deps', []):
+                dep_artifact_id, dep_before = visit(dep)
+                imports.append({'ref': dep.upper(), 'id': dep_artifact_id, 'before': dep_before})
+                before.append(dep_artifact_id)
+            artifact_id = build_package(self, pkg, imports)
+            result = (artifact_id, before)
+            built[name] = result
+            return result
+
+        artifact_id, before_list = visit(root_name)
+        return artifact_id
+
 
 def download_sources(ctx, pkg):
     """
@@ -34,7 +62,8 @@ def build_package(ctx, pkg, imports):
     script = []
     buildspec = dict(name=pkg['package'],
                      version='n',
-                     build={'script': script, 'import': imports, 'env': {}})
+                     build={'script': script, 'import': imports, 'env': ctx.build_env},
+                     files=[])
 
     # Listing the sources to unpack. We want to have:
     #   - $BUILD/src: the tarball/git commit listed in package.json,
@@ -51,65 +80,75 @@ def build_package(ctx, pkg, imports):
         files[base] = contents
     files_key = ctx.source_cache.put(files)
 
-    buildspec['sources'] = [
-        dict(key=files_key, target='.'),
-        dict(key=pkg['key'], target='src',
-             strip=0 if pkg['key'].startswith('git:') else 1)
-        ]
-    script.append(["@hdist", "build-unpack-sources"])
+    if 'key' in pkg:
+        buildspec['sources'] = [
+            dict(key=files_key, target='.'),
+            dict(key=pkg['key'], target='src',
+                 strip=0 if pkg['key'].startswith('git:') else 1)
+            ]
+        script.append(["@hdist", "build-unpack-sources"])
 
     # Environment:
 
     recipe_func = getattr(recipes_mod, '%s_recipe' % pkg.get('recipe', 'standard'))
-    recipe_func(ctx, pkg, buildspec)
+    recipe_func(ctx, pkg, files, buildspec)
 
     if not ctx.build_store.is_present(buildspec):
-        download_sources(ctx, pkg)
+        if 'key' in pkg:
+            download_sources(ctx, pkg)
+        ctx.logger.info('Building %s' % pkg['package'])
+    else:
+        ctx.logger.info('Up to date: %s' % pkg['package'])
     artifact_id, dir = ctx.build_store.ensure_present(buildspec, ctx.config, keep_build='error')
-    ctx.logger.info('Building %s' % pkg['package'])
     return artifact_id
 
-def build_all(ctx, packages, subset):
-    built = {} # { name : (artifact_id, before_list) }
-    # depth-first traversal
-    def visit(name):
-        if name in built:
-            return built[name]
-        pkg = packages[name]
-        imports = [] # the imports to build this package
-        before = [] # to be given to dependee, which puts this in before-list for this artifact
-        for dep in pkg.get('deps', []):
-            dep_artifact_id, dep_before = visit(dep)
-            imports.append({'ref': dep.upper(), 'id': dep_artifact_id, 'before': dep_before})
-            before.append(dep_artifact_id)
-        artifact_id = build_package(ctx, pkg, imports)
-        result = (artifact_id, before)
-        built[name] = result
-        return result
-
-    for package in subset:
-        visit(package)
-    
+def complete_dependencies(packages, subset):
+    """
+    Given a package database `packages` and a list of package names `subset`,
+    return `subset` extended so that all ultimate dependencies are included.
+    """
+    visited = set()
+    result = []
+    def search(pkgname):
+        if pkgname not in visited:
+            for dep in packages[pkgname].get('deps', []):
+                search(dep)
+            visited.add(pkgname)
+            result.append(pkgname)
+    for root in subset:
+        search(root)
+    return result
 
 def main():
     # Parse arguments
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-v', '--verbose', help='verbose', action='store_true')
-    argparser.add_argument('arch', help='e.g., "linux"')
-    argparser.add_argument('subset', nargs='*', help='only attempt to build packages given '
-                           '(+ their dependencies)')
+    #argparser.add_argument('arch', help='e.g., "linux"')
+    #argparser.add_argument('subset', nargs='*', help='only attempt to build packages given '
+    #                       '(+ their dependencies)')
     args = argparser.parse_args()
 
+    with open('config.yml') as f:
+        hpcmp_config = yaml.safe_load(f)
+
+
+    env = {'PATH': ':'.join(hpcmp_config['PATH'])}
+    arch = hpcmp_config['arch']
+    target_link = pjoin('profiles', arch)
+    silent_makedirs('profiles')
+    
     # Set up Hashdist components, configured by ./hdistconfig
-    config = load_configuration_from_inifile('./hdistconfig')
-    ctx = Context(config, args.verbose, args.arch)
+    hdist_config = load_configuration_from_inifile('./hdistconfig')
+    ctx = Context(hdist_config, args.verbose, arch, env)
 
     with open('packages.yml') as f:
         package_list = yaml.safe_load(f)
     packages = dict((pkg['package'], pkg) for pkg in package_list)
+    subset = hpcmp_config.get('packages', packages.keys())
+    subset = complete_dependencies(packages, subset)
 
-    if not args.subset:
-        args.subset = packages.keys()
-
-    build_all(ctx, packages, args.subset)
-
+    packages['profile'] = {'package': 'profile', 'recipe': 'profile',
+                           'deps': subset}
+    profile_aid = ctx.build_all(packages, 'profile')
+    profile_path = ctx.build_store.resolve(profile_aid)
+    atomic_symlink(profile_path, target_link)
--- builder/recipes.py
+++ builder/recipes.py
@@ -2,11 +2,46 @@
 # is taken from packages.yml
 import os
 
-def standard_recipe(ctx, attrs, buildspec):
-    buildspec['build']['script'].append([ # appending a list creates a sub-scope
+def add_profile_install(build_spec):
+    artifact_spec_file = {
+        "target": "$ARTIFACT/artifact.json",
+        "object": {
+            "install": {
+                "script": [["@hdist", "create-links", "--key=install/links", "artifact.json"]],
+                "links": [{"action": "symlink",
+                           "select": "$ARTIFACT/*/**/*",
+                           "prefix": "$ARTIFACT",
+                           "target": "$PROFILE"}]
+                }
+            }
+        }
+    build_spec['files'].append(artifact_spec_file)
+    build_spec['build']['script'].insert(0, ['@hdist', 'build-write-files'])
+
+
+def standard_recipe(ctx, attrs, configfiles, build_spec):
+    script = [
         ['cd', 'src'],
-        ['PATH=%s' % os.environ['PATH']],
-        ['PYTHONHPC_PREFIX=${ARTIFACT}'],
-        ['sh', '../configure'],
+        ["CC=/usr/bin/ccache /usr/bin/gcc"],
+        ['PYTHONHPC_PREFIX=${ARTIFACT}']
+        ]
+    if 'configure' in configfiles:
+        script += [['sh', '../configure']]
+    script += [
         ['make'],
-        ['make', 'install']])
+        ['make', 'install']
+        ]
+    build_spec['build']['script'].append([script]) # make a sub-scope for above comments
+    add_profile_install(build_spec)
+
+def profile_recipe(ctx, attrs, configfiles, build_spec):
+    # emit 'profile' section in build spec
+    profile = []
+    for dep in attrs['deps']:
+        before = ctx.get_before_list(dep)
+        profile.append({"id": ctx.get_artifact_id(dep), "before": before})
+    build_spec['profile'] = profile
+
+    # emit command to create profile
+    cmd = ["hdist", "create-profile", "--key=profile", "build.json", "$ARTIFACT"]
+    build_spec['build']['script'].append(cmd)
